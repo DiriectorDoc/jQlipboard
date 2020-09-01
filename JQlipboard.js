a66cb4bb0e52ab65fd61bae08497e92cf692a7d7
DiriectorDoc <66105586+DiriectorDoc@users.noreply.github.com>
2020-08-31 22:18:21 -0400

    Whitespaces → tabs

--- JQlipboard.js
+++ JQlipboard.js
@@ -1,7 +1,7 @@
 (function($) {
-	
+
 	if(!$) return;
-	
+
 	window.qlipboard = {};
 
 	const isIE = !!document.documentMode; // IE browser exclusive. Checks if using IE
@@ -18,7 +18,7 @@
 			window.qlipboard.text = $this[0] && "IMG" == $this[0].tagName ? "image" : $this.val() || $this.html() || window.qlipboard.clipboard;
 		}
 	}
-	
+
 	function warning(){
 		navigator.permissions.query({name: "clipboard-read"}).then(result => {
 			if(result.state == "denied"){
@@ -28,164 +28,164 @@
 				)
 			}
 		});
-		
+
 		warning = nothing
 	}
-	
+
 	function nothing(){
 		return
 	}
 
-    $.fn.copy = function(){
-        if(this.parent().length){
-            this.select()
-            $.copy()
-            setQlipboard(this)
-            if(this.css("user-select") === "none"){
-                $.copy(this.val() || this.html())
-            }
-            return this
-        } else {
-            return this
+	$.fn.copy = function(){
+		if(this.parent().length){
+			this.select()
+			$.copy()
+			setQlipboard(this)
+			if(this.css("user-select") === "none"){
+				$.copy(this.val() || this.html())
+			}
+			return this
+		} else {
+			return this
 				.css({
-                    position: "absolute",	// Ensures that appending the object does not mess up the existing document
-                    opacity: 0,				// ↴
-                    color: "rgba(0,0,0,0)",	// Makes the object invisible. `display:none` will not work since it disables the avility to select it
-                    "-webkit-user-select": "auto",
-                    "-khtml-user-select": "auto",
-                    "-moz-user-select": "auto", // Ensures that the appended object can be selected, just in case it was disabled in the stylesheet
-                    "-ms-user-select": "auto",
-                    "user-select": "auto"
-                })
-                .appendTo("body")
-                .copy()
-                .remove()
-        }
-    };
+					position: "absolute",   // Ensures that appending the object does not mess up the existing document
+					opacity: 0,             // ↴
+					color: "rgba(0,0,0,0)", // Makes the object invisible. `display:none` will not work since it disables the avility to select it
+					"-webkit-user-select": "auto",
+					"-khtml-user-select": "auto",
+					"-moz-user-select": "auto",	// Ensures that the appended object can be selected, just in case it was disabled in the stylesheet
+					"-ms-user-select": "auto",
+					"user-select": "auto"
+				})
+				.appendTo("body")
+				.copy()
+				.remove()
+		}
+	};
 
 	//needs an overhaul
-    $.fn.paste = function(selector, method){
-        if(!method){
-            if((selector == "element" && !$("body").find("element").length) && (selector == "text" && !$("body").find("text").length)){
-                method = selector;
-                selector = 0;
-            }
-            method = "text";
-        }
-        if(this.attr("qlip-cut") || selector){ // If `this` is a cut element, there needs to be a selector so it can be pasted somewhere
-            if($("body").find(selector).length){
-                let sel = $(selector),
-                    tag = sel[0].tagName;
-                if("INPUT" == tag || "TEXTAREA" == tag){
-                    return sel.val(window.qlipboard.text)
-                } else if("IMG" == tag){
-                    return sel.append(window.qlipboard.jqobj.clone())
-                } else {
-                    return method == "element" ? sel.append(window.qlipboard.jqobj.clone().removeAttr("qlip-cut")) : method == "text" ? sel.html(window.qlipboard.text) : sel
-                }
-            } else {
-                console.warn('Could not paste item\nUnable to find element that matched selector "' + selector + '"')
-                return this;
-            }
-        }
-        let tag = this[0].tagName;
-        if("INPUT" == tag || "TEXTAREA" == tag){
-            return this.val(window.qlipboard.text)
-        } else if("IMG" == tag){
-            return this.append(window.qlipboard.jqobj.clone())
-        } else {
-            return method == "element" ? this.append(window.qlipboard.jqobj.clone().removeAttr("qlip-cut")) : method == "text" ? this.html(window.qlipboard.text) : this
-        }
-    };
+	$.fn.paste = function(selector, method){
+		if(!method){
+			if((selector == "element" && !$("body").find("element").length) && (selector == "text" && !$("body").find("text").length)){
+				method = selector;
+				selector = 0;
+			}
+			method = "text";
+		}
+		if(this.attr("qlip-cut") || selector){ // If `this` is a cut element, there needs to be a selector so it can be pasted somewhere
+			if($("body").find(selector).length){
+				let sel = $(selector),
+					tag = sel[0].tagName;
+				if("INPUT" == tag || "TEXTAREA" == tag){
+					return sel.val(window.qlipboard.text)
+				} else if("IMG" == tag){
+					return sel.append(window.qlipboard.jqobj.clone())
+				} else {
+					return method == "element" ? sel.append(window.qlipboard.jqobj.clone().removeAttr("qlip-cut")) : method == "text" ? sel.html(window.qlipboard.text) : sel
+				}
+			} else {
+				console.warn('Could not paste item\nUnable to find element that matched selector "' + selector + '"')
+				return this;
+			}
+		}
+		let tag = this[0].tagName;
+		if("INPUT" == tag || "TEXTAREA" == tag){
+			return this.val(window.qlipboard.text)
+		} else if("IMG" == tag){
+			return this.append(window.qlipboard.jqobj.clone())
+		} else {
+			return method == "element" ? this.append(window.qlipboard.jqobj.clone().removeAttr("qlip-cut")) : method == "text" ? this.html(window.qlipboard.text) : this
+		}
+	};
 
-    $.fn.cut = function(){
-        return this
-            .attr("qlip-cut", true)
-            .copy()
-            .remove()
-    };
+	$.fn.cut = function(){
+		return this
+			.attr("qlip-cut", true)
+			.copy()
+			.remove()
+	};
 
-    const $select = $.fn.select;
-    $.fn.select = function(elem, name, value, pass){
-        if("INPUT" == this[0].tagName || "TEXTAREA" == this[0].tagName){
-            return $select(elem, name, value, pass)
+	const $select = $.fn.select;
+	$.fn.select = function(elem, name, value, pass){
+		if("INPUT" == this[0].tagName || "TEXTAREA" == this[0].tagName){
+			return $select(elem, name, value, pass)
 		} else if(document.selection){
-            let range = document.body.createTextRange();
-            range.moveToElementText(this[0])
-            range.select().createTextRange()
-        } else if(window.getSelection){
-            let range = document.createRange(),
-                selec = window.getSelection();
-            range.selectNode(this[0])
-            selec.removeAllRanges()
-            selec.addRange(range)
-        }
-        return this;
-    };
+			let range = document.body.createTextRange();
+			range.moveToElementText(this[0])
+			range.select().createTextRange()
+		} else if(window.getSelection){
+			let range = document.createRange(),
+				selec = window.getSelection();
+			range.selectNode(this[0])
+			selec.removeAllRanges()
+			selec.addRange(range)
+		}
+		return this;
+	};
 
-    $.cut = function(){
-        try {
-            if(isIE){
-                throw false;
-            } else {
-                document.execCommand("cut")
-            }
-        } catch(err){
+	$.cut = function(){
+		try {
+			if(isIE){
+				throw false;
+			} else {
+				document.execCommand("cut")
+			}
+		} catch(err){
 			if(err){
 				console.error(err)
 			}
-            $.copy()
-            let focus = $(":focus"),
+			$.copy()
+			let focus = $(":focus"),
 				e = focus[0],
 				text = focus.val();
 			text = text.slice(0, e.selectionStart) + text.slice(e.selectionEnd);
 			focus.val(text)
-        }
-    };
+		}
+	};
 
-    $.copy = function(text){
-        if(text !== undefined){
-            $("<a>")
-                .html(text)
-                .copy()
-            window.qlipboard.jqobj = null;
-        } else {
-            try {
-                document.execCommand("copy")
-            } catch(err){
-                console.error(err)
-                console.warn("Trying navigator.clipboard.writeText() instead")
-                let text = "";
-                if(window.getSelection){
-                    text = window.getSelection().toString();
-                } else if(document.selection && document.selection.type != "Control"){
-                    text = document.selection.createRange().text;
-                }
+	$.copy = function(text){
+		if(text !== undefined){
+			$("<a>")
+				.html(text)
+				.copy()
+			window.qlipboard.jqobj = null;
+		} else {
+			try {
+				document.execCommand("copy")
+			} catch(err){
+				console.error(err)
+				console.warn("Trying navigator.clipboard.writeText() instead")
+				let text = "";
+				if(window.getSelection){
+					text = window.getSelection().toString();
+				} else if(document.selection && document.selection.type != "Control"){
+					text = document.selection.createRange().text;
+				}
 				warning()
-                navigator.clipboard.writeText(text)
-            }
-        }
-    };
+				navigator.clipboard.writeText(text)
+			}
+		}
+	};
 
-    $.paste = function(){
-        if(isIE){
-            document.execCommand("paste")
-        } else {
+	$.paste = function(){
+		if(isIE){
+			document.execCommand("paste")
+		} else {
 			warning()
-            navigator.clipboard.readText()
-                .then(clipText => {
-                    let focus = $(":focus"),
-                        e = focus[0],
-                        text = focus.val();
-                    text = text.slice(0, e.selectionStart) + clipText + text.slice(e.selectionEnd);
-                    focus.val(text)
-                })
-                .catch(err => {
-                    console.error("Could not execute paste", err)
-                })
-        }
-    };
-	
+			navigator.clipboard.readText()
+				.then(clipText => {
+					let focus = $(":focus"),
+						e = focus[0],
+						text = focus.val();
+					text = text.slice(0, e.selectionStart) + clipText + text.slice(e.selectionEnd);
+					focus.val(text)
+				})
+				.catch(err => {
+					console.error("Could not execute paste", err)
+				})
+		}
+	};
+
 	$.Qlipboard = function(config){
 		config = {
 			permissionPrompt: config.permissionPrompt || "when needed",
